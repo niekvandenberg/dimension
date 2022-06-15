1da0e9a7a101f7f1b791ed7481f67ffd38bc3664
niekvandenberg <niek2004@icloud.com>
2022-06-15 10:10:02 +0200

    added

---
+++ src/main/java/com/niek/dimension/DimensionClient.java
@@ -0,0 +1,10 @@
+package com.niek.dimension;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DimensionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
